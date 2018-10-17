517f58c710feee2ddbe222637f5c3f772a5f1a54
vnikic <vnikic@gmail.com>
2018-10-17 17:15:48 +0200

    Update

---
+++ dynamic programming/subset_sum.js
@@ -0,0 +1,89 @@
+/*
+Given a set of non-negative integers, and a value sum, determine if there is a subset of the
+given set with sum equal to given sum.
+*/
+
+let naiveRecursiveGetSubsetForSum = (array, fromIndex, sum, subArray) => {
+    if (sum === 0) {
+        return subArray;
+    } else {
+        for (let i = fromIndex; i < array.length; i++) {
+            let curr = array[i];
+            if (curr <= sum) {
+                subArray.push(curr);
+                let result = naiveRecursiveGetSubsetForSum(array, i + 1, sum - curr, subArray);
+                if (result != null) {
+                    return result;
+                }
+                subArray.pop();
+            }
+        }
+    }
+    return null;
+};
+
+
+let naiveSolution = (array, sum) => {
+    return naiveRecursiveGetSubsetForSum(array, 0, sum, []);
+};
+
+
+let memoize = (array, sum) => {
+    let m = [];
+    for (let i = 0; i < array.length; i++) {
+        m[i] = [];
+        let curr = array[i];
+        for (let j = 0; j <= sum; j++) {
+            if (i === 0) {
+                m[i][j] = curr === j ? 2 : 0;
+            } else {
+                if (curr === j) {
+                    m[i][j] = 2;
+                } else if (curr < j && m[i - 1][j - curr] > 0) {
+                    m[i][j] = 2;
+                } else {
+                    m[i][j] = m[i - 1][j] === 2 ? 1 : m[i - 1][j];
+                }
+            }
+        }
+    }
+    return m;
+};
+
+let findSubarrayForSum = (array, sum, table) => {
+    // first find first 1 in column sum
+    for (let i = 0; i < array.length; i++) {
+        if (table[i][sum] === 2) {
+            let result = [];
+            let x = i;
+            while (x >= 0) {
+                if (table[x][sum] === 2) {
+                    result.unshift(array[x]);
+                    sum -= array[x];
+                }
+                x--;
+            }
+            return result;
+        }
+    }
+    return null;
+};
+
+
+
+
+// TESTING
+
+let array = [3, 34, 4, 12, 5, 2];
+let maxSum = 100;
+
+console.log( naiveSolution(array, 10) );
+
+let table = memoize(array, maxSum);
+console.log(findSubarrayForSum(array, 8, table));
+console.log(findSubarrayForSum(array, 10, table));
+console.log(findSubarrayForSum(array, 12, table));
+console.log(findSubarrayForSum(array, 14, table));
+console.log(findSubarrayForSum(array, 18, table));
+console.log(findSubarrayForSum(array, 21, table));
+console.log(findSubarrayForSum(array, 56, table));
