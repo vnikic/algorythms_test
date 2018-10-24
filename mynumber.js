32a4d67f0bbdd03f7a43b1d672b99183067d15a0
vnikic <vnikic@gmail.com>
2018-10-24 17:19:42 +0200

    Update

--- mynumber.js
+++ mynumber.js
@@ -1,26 +1,135 @@
-let NodeOption = {
-    left: ["0"],
-    right: ["1"],
-    leftandright: ["0", "1"],
-    none: []
-};
+let OPERATOR_NONE = 0;
+let OPERATOR_PLUS = 1;
+let OPERATOR_MINUS = 2;
+let OPERATOR_MULTIPLICATION = 3;
+let OPERATOR_DIVIDE = 4;
 
-let makeTrees = (nodeCount, path, nodes, initialNodeCount) => {
-    if (nodeCount === 0 && nodes.length === initialNodeCount) {
-        console.log(nodes);
-    } else {
-        nodeCount--;
-        for (let i = 0; i <= nodeCount; i++) {
-            let leftToPush = nodeCount - i;
-            let rightToPush = i;
-            makeTrees(leftToPush, path + "0", nodes.concat([path]), initialNodeCount);
-            makeTrees(rightToPush, path + "1", nodes.concat([path]), initialNodeCount);
+let ALL_OPERATORS = [OPERATOR_NONE, OPERATOR_PLUS, OPERATOR_MINUS, OPERATOR_MULTIPLICATION, OPERATOR_DIVIDE];
+
+class CalcNode {
+    constructor() {
+        this.left = null;
+        this.right = null;
+    }
+
+    calc() {
+        return NaN;
+    }
+
+    setLeft(node) {
+        this.left = node;
+    }
+
+    setRight(node) {
+        this.right = node;
+    }
+
+    isLeaf() {
+        return false;
+    }
+
+    setNodes(left, right) {
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class OperatorNode extends CalcNode {
+    setOperator(op) {
+        this.operator = op;
+    }
+
+    calc() {
+        switch (this.operator) {
+            case OPERATOR_NONE:
+                if (this.left) {
+                    return this.left.calc();
+                }
+                break;
+            case OPERATOR_PLUS:
+                if (this.left && this.right) {
+                    return this.left.calc() + this.right.calc();
+                }
+                break;
+            case OPERATOR_MINUS:
+                if (this.left && this.right) {
+                    return this.left.calc() - this.right.calc();
+                }
+                break;
+            case OPERATOR_MULTIPLICATION:
+                if (this.left && this.right) {
+                    return this.left.calc() * this.right.calc();
+                }
+                break;
+            case OPERATOR_DIVIDE:
+                if (this.left && this.right) {
+                    let x = this.right.calc();
+                    return x !== 0 ? this.left.calc() / x : NaN;
+                }
+                break;
+        }
+        return NaN;
+    }
+}
+
+
+class NumberNode extends CalcNode {
+    constructor(num) {
+        super();
+        this.num = num;
+    }
+
+    setNumber(num) {
+        this.num = num;
+    }
+
+    isLeaf() {
+        return true;
+    }
+
+    setLeft(node) {
+    }
+
+    setRight(node) {
+    }
+
+    setNodes(left, right) {
+    }
+
+    calc() {
+        return this.num ? this.num: NaN;
+    }
+}
+
+
+let createCalcTree = (nodes) => {
+    let rootNode = null;
+    for (let i = 0; i < nodes.length; i++) {
+        let currNode = nodes[i];
+        if (currNode === "") {
+            rootNode = new OperatorNode();
+        } else {
+            let nodeToInsertInto = rootNode;
+            for (let i = 0; i < currNode.length - 1; i++) {
+                if (currNode[i] === "0") {
+                    nodeToInsertInto = nodeToInsertInto.left;
+                } else {
+                    nodeToInsertInto = nodeToInsertInto.right;
+                }
+            }
+            let last = currNode[currNode.length - 1];
+            if (last === "0") {
+                nodeToInsertInto.setLeft(new OperatorNode());
+            } else {
+                nodeToInsertInto.setRight(new OperatorNode());
+            }
         }
     }
+    return rootNode;
 };
 
-
-let create = (n, nodes) => {
+let createCalcTrees = (n, nodes, calcTrees) => {
     if (n > 0) {
         for (let i = 0; i < nodes.length; i++) {
             let nodeToAppend = nodes[i];
@@ -28,16 +137,46 @@ let create = (n, nodes) => {
                 let newNode = nodeToAppend + j;
                 if (nodes[nodes.length - 1] < newNode) {
                     nodes.push(newNode);
-                    create(n - 1, nodes);
+                    createCalcTrees(n - 1, nodes, calcTrees);
                     nodes.pop();
                 }
             }
         }
     } else {
-        console.log(nodes);
+        calcTrees.push(createCalcTree(nodes));
     }
 };
 
-// makeTrees(3, "", [], 3);
+let addNumberToCalcTree = (calcTree, num) => {
+    let inserted  = false;
+    if (!calcTree.isLeaf() && calcTree.left === null) {
+        calcTree.setLeft(new NumberNode(num));
+        inserted  = true;
+    } else if (!calcTree.isLeaf() && calcTree.right === null) {
+        calcTree.setRight(new NumberNode(num));
+        inserted  = true;
+    }
+    if (!inserted) {
+        inserted = addNumberToCalcTree(calcTree.left, num);
+        if (!inserted) {
+            inserted = addNumberToCalcTree(calcTree.right, num);
+        }
+    }
+    return inserted;
+};
 
-create(3, ["0"]);
+let addNumbersToCalcTree = (calcTree, numbers) => {
+    for (let i = 0; i < numbers.length; i++) {
+        addNumberToCalcTree(calcTree, numbers[i]);
+    }
+};
+
+
+let calcTrees = [];
+createCalcTrees(4, [""], calcTrees);
+
+addNumbersToCalcTree(calcTrees[0], [10, 5, 76, 34, 11, 22]);
+
+console.log(calcTrees[0]);
+
+
