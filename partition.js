ba8a6d87f825481012b5a988273a469d96dd7374
vnikic <nikic_vladimir@yahoo.com>
2019-06-19 11:22:50 +0200

    Small updates

--- partition.js
+++ partition.js
@@ -1,20 +1,28 @@
-function partRecursive(n, max, partArr, index) {
-    if (n == 0) {
-        console.log( partArr.slice(0, index) );
-    } else {
-        for (var i = max; i >= 1; i--) {
-            var m = n - i;
-            if (m >= 0) {
-                partArr[index] = i;
-                partRecursive(m, i, partArr, index + 1);
+/**
+ * Generate all partitions for the given integer. Partitions are combinations of a sum number.
+ * @param n Integer to be partitioned
+ * @partFunc Function to apply to partition array
+ */
+let partition = (n, partFunc) => {
+    let partitionRecursive = (n, max, partArr, index) => {
+        if (n === 0) {
+            if (partFunc) {
+                partFunc( partArr.slice(0, index) );
+            }
+        } else {
+            for (let i = max; i >= 1; i--) {
+                let m = n - i;
+                if (m >= 0) {
+                    partArr[index] = i;
+                    partitionRecursive(m, i, partArr, index + 1);
+                }
             }
         }
-    }
-}
+    };
 
-function part(n) {
-    partRecursive(n, n, [], 0);
-}
+    partitionRecursive(n, n, [], 0);
+};
 
 
-part(7);
+// test
+partition( 7, (a) => console.log(a) );
